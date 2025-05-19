1fe563305b059b84e27db69c66a15b4d5d56f307
jtestor <jtestor@alumnos.uai.cl>
2025-05-19 19:56:21 -0400

    Initial Commit

---
+++ Demo App TID/Demo_App_TIDApp.swift
@@ -0,0 +1,17 @@
+//
+//  Demo_App_TIDApp.swift
+//  Demo App TID
+//
+//  Created by Miguel Testor on 19-05-25.
+//
+
+import SwiftUI
+
+@main
+struct Demo_App_TIDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Demo App TIDTests/Demo_App_TIDTests.swift
@@ -0,0 +1,17 @@
+//
+//  Demo_App_TIDTests.swift
+//  Demo App TIDTests
+//
+//  Created by Miguel Testor on 19-05-25.
+//
+
+import Testing
+@testable import Demo_App_TID
+
+struct Demo_App_TIDTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
